8c1dbe092298bd893aae32f02c19669a4a3c556f
JordanRaleigh <jraleigh1127@gmail.com>
2020-06-26 11:29:12 -0500

    test

--- server/api/categories.js
+++ server/api/categories.js
@@ -3,7 +3,7 @@ const { Account, Category, Perk, CreditCard } = require('../db');
 
 router.get('/', async (req, res, next) => {
   try {
-    console.log('helloAPI:');
+    console.log('helloAPI:ghjk');
     const categories = await Category.findAll();
     res.status(200).json(categories);
   } catch (error) {
